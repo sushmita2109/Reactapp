87160c216742981b8b2b95a61e5f1c5db6488bc3
OxfordTrail <lakshyaing@gmail.com>
2020-08-28 08:46:14 +0530

    Updated Modal to appear on Button click.

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -1,33 +1,31 @@
 import React, { Component } from 'react';
-import {  Collapse, Button, Modal, ModalHeader, ModalBody, Form, FormGroup, Input, Label } from 'reactstrap';
+import { Button, Modal, ModalHeader } from 'reactstrap';
 
-class  CommentForms extends Component{
-    constructor (props){
-      super(props);
-      this.state={
-        show:false
-      };
-      
-    }
-     
+class CommentForms extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false
+    };
+  }
 
-    handleShow = () => {
-      console.log("I'm hit")
-      this.setState({show:true});
-    }
-    
-    render(){
-      console.log(this.state.show)
-      return (<>
-        <Button onClick={this.handleShow}>feedback</Button>
-        
-         <Modal show={this.state.show} >
-           <ModalHeader>
-             hiii
+
+  handleShow = () => {
+    this.setState({ show: true });
+  }
+
+  render() {
+    return (
+    <>
+      <Button onClick={this.handleShow}>Feedback</Button>
+
+      <Modal isOpen={this.state.show} >
+        <ModalHeader>
+          hiii
            </ModalHeader>
-         </Modal>
-       
-       </>)
-    }
+      </Modal>
+
+    </>)
+  }
 }
 export default CommentForms;
